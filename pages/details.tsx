6607905f4c0b1f3e4ebed54ed222a10730cbd0ba
Seung-ho Kim <rhanfwlq980@naver.com>
2022-08-02 00:15:38 +0900

    Ch4.8 : More Modifiers

---
+++ pages/details.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Details: NextPage = () => {
+  return (
+    <div className={"flex flex-col space-y-2 p-5"}>
+      <p className={"first-letter:text-7xl first-letter:hover:text-purple-400"}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus asperiores corporis cum, earum, eum, exercitationem ipsa ipsum maiores maxime minus nesciunt non optio provident ratione sequi soluta tenetur voluptates voluptatum?</p>
+    </div>
+  );
+}
+
+export default Details;
