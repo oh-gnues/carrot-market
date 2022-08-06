0a0ceacbcc44fa8a7bd3ca820313856ee4c4167d
Seung-ho Kim <rhanfwlq980@naver.com>
2022-08-06 14:48:38 +0900

    Ch #5.17-5.18 Layout

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,8 +1,5 @@
 import {useState} from "react";
-
-function cls(...classnames: string[]) {
-  return classnames.join(" ");
-}
+import {cls} from "../libs/utils";
 
 export default function Enter() {
   const [method, setMethod] = useState<"email" | "phone">("email");
@@ -41,13 +38,14 @@ export default function Enter() {
           </div>
         </div>
         <form className={"flex flex-col mt-8"}>
-          <label className={"text-sm font-medium text-gray-700"}>
+          <label htmlFor={"input"} className={"text-sm font-medium text-gray-700"}>
             {method === "email" ? "Email address" : null}
             {method === "phone" ? "Phone number" : null}
           </label>
           <div className={"mt-1"}>
             {method === "email" ? (
               <input
+                id={"input"}
                 type={"email"}
                 className={"appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"}
                 required
@@ -57,6 +55,7 @@ export default function Enter() {
               <div className={"flex rounded-md shadow-sm"}>
                 <span className={"flex items-center justify-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 select-none text-sm"}>+82</span>
                 <input
+                  id={"input"}
                   type={"number"}
                   className={"appearance-none w-full px-3 py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"}
                   required
