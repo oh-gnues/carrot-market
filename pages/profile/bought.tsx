54e4c1ecfa9193bb88ad09b70cd550a397bd527b
Seung-ho Kim <rhanfwlq980@naver.com>
2022-08-06 16:39:16 +0900

    profile 페이지 리팩터링

---
+++ pages/profile/bought.tsx
@@ -0,0 +1,17 @@
+import {NextPage} from "next";
+import Layout from "../../components/layout";
+import Item from "../../components/item";
+
+const Bought: NextPage = () => {
+  return (
+    <Layout canGoBack title={"구매내역"}>
+      <div className={"flex flex-col space-y-5 pb-10 divide-y"}>
+        {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+          <Item key={i} id={i} title={"iPhone 14"} price={99} comments={1} hearts={1} />
+        ))}
+      </div>
+    </Layout>
+  );
+};
+
+export default Bought;
