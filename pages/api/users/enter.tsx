028fe74200d1ef6ac5d24b881cb85ca43e6b97ff
Seung-ho Kim <rhanfwlq980@naver.com>
2022-08-28 19:25:21 +0900

    Ch9.2 Token Logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,23 +1,29 @@
-import {NextApiRequest, NextApiResponse} from "next";
-import withHandler from "@libs/server/withHandler";
-import client from "@libs/server/client";
+import { NextApiRequest, NextApiResponse } from 'next'
+import withHandler from '@libs/server/withHandler'
+import client from '@libs/server/client'
 
-async function handler (req:NextApiRequest, res:NextApiResponse) {
-	const { phone, email } = req.body;
-	const payload = phone ? { phone: +phone } : { email };
-	const user = await client.user.upsert({
-		where: {
-			...payload,
-		},
-		create: {
-			name: "Anonymous",
-			...payload,
-		},
-		update: {},
-	});
-	console.log(user);
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { phone, email } = req.body
+  const payload = phone ? { phone: +phone } : { email }
+  const token = await client.token.create({
+    data: {
+      payload: '1234',
+      user: {
+        connectOrCreate: {
+          where: {
+            ...payload,
+          },
+          create: {
+            name: 'Anonymous',
+            ...payload,
+          },
+        },
+      },
+    },
+  })
+  console.log(token)
 
-	/*if (email) {
+  /*if (email) {
 		user = await client.user.findUnique({
 			where: {
 				email,
@@ -55,7 +61,7 @@ async function handler (req:NextApiRequest, res:NextApiResponse) {
 		console.log(user);
 	}*/
 
-	return res.status(200).end();
+  return res.status(200).end()
 }
 
-export default withHandler("POST", handler);
+export default withHandler('POST', handler)
